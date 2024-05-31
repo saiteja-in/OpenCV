907aa6170eb566c7145156af1b50020fde8fdfcd
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-31 19:58:56 +0530

    flip 4 images effect

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ while True:
 
     image = np.zeros(frame.shape, np.uint8)
     smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
+    image[:height//2, :width//2] = cv2.rotate(cv2.flip(smaller_frame,1), cv2.ROTATE_180)
     image[height//2:, :width//2] = smaller_frame
-    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
-    image[height//2:, width//2:] = smaller_frame
+    image[:height//2, width//2:] = cv2.rotate(smaller_frame,1, cv2.ROTATE_180)
+    image[height//2:, width//2:] = cv2.flip(smaller_frame,1)
 
     cv2.imshow('frame', image)
 
