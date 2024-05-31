e39aaa50f344cea69be0e59f977549561acb479e
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-31 20:31:41 +0530

    adding texts,lines,shapes on image

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ while True:
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    image = np.zeros(frame.shape, np.uint8)
-    smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-    image[:height//2, :width//2] = cv2.rotate(cv2.flip(smaller_frame,1), cv2.ROTATE_180)
-    image[height//2:, :width//2] = smaller_frame
-    image[:height//2, width//2:] = cv2.rotate(smaller_frame,1, cv2.ROTATE_180)
-    image[height//2:, width//2:] = cv2.flip(smaller_frame,1)
+    img = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 10)
+    img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
+    img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5)
+    img = cv2.circle(img, (300, 300), 60, (0, 0, 255), -1)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    img = cv2.putText(img, 'SAI TEJA!', (10, height - 10), font, 2, (0, 0, 0), 5, cv2.LINE_AA)
 
-    cv2.imshow('frame', image)
+    cv2.imshow('frame', img)
 
     if cv2.waitKey(1) == ord('q'):
         break
