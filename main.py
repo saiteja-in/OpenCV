0fb77e2ec947376d918fdcaa0895d3e4d13b1174
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-31 17:46:30 +0530

    adding pixels to an image

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import cv2
+import random
 img = cv2.imread('assets/logo.jpg',-1)
 print(img.shape[1])
-print(img.shape)#(995, 1000, 3)
+print(img.shape)#(995, 1000, 3(channels))
+
+for i in range(100):
+	for j in range(img.shape[1]):
+		img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+
+  
+tag = img[500:700, 600:900]
+img[100:300, 650:950] = tag  
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
-# img.shape returns the dimensions of the image in the form of a tuple (height, width). Therefore, img.shape[1] specifically refers to the width of the image.
-
-# cv2.imshow('Image', img)
-# cv2.waitKey(0)
+# blue green red
+# [255,0,0]means only blue pixel is present
+# [255,255,0] both blue and gree are present
