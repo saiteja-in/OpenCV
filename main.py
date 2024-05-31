6322a4950073004a5c7f521c35661b42d8b69494
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-31 18:03:03 +0530

    reading from a video

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
+import numpy as np
 import cv2
-import random
-img = cv2.imread('assets/logo.jpg',-1)
-print(img.shape[1])
-print(img.shape)#(995, 1000, 3(channels))
 
-for i in range(100):
-	for j in range(img.shape[1]):
-		img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+cap=cv2.VideoCapture(0)
 
-  
-tag = img[500:700, 600:900]
-img[100:300, 650:950] = tag  
-cv2.imshow('Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-# blue green red
-# [255,0,0]means only blue pixel is present
-# [255,255,0] both blue and gree are present
+while True:
+    ret,frame=cap.read()
+    
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows() 
